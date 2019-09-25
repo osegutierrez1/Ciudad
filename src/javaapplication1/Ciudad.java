2b7cc0930952b21daf6cb700429b6044bbaa5029
jose.gutierrez <51164721+osegutierrez1@users.noreply.github.com>
2019-09-25 12:59:14 -0500

    version 1.0
    crear atributos

---
+++ src/javaapplication1/Ciudad.java
@@ -0,0 +1,12 @@
+package Ciudad;
+
+import java.util.Date;
+
+public class Ciudad {
+        
+     private int numeroDeCiudadanos;
+     private Date fechaDeFundacion;
+     private String nombreDeLaCiudad;
+     private boolean contaminacion;     
+     
+}
